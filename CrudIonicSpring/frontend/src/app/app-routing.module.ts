bd182e1db7746ae39ab0dac3214c0d6dc1c278f4
alarcon_osorio <alarcon_osorio@hotmail.com>
2020-06-24 00:44:46 +0000

    IonicCrudSpring

---
+++ CrudIonicSpring/frontend/src/app/app-routing.module.ts
@@ -0,0 +1,50 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },  
+  {
+    path: 'admin',
+    loadChildren: () => import('./pages/admin/admin.module').then( m => m.AdminPageModule)
+  },
+  {
+    path: 'editar-producto',
+    loadChildren: () => import('./pages/editar-producto/editar-producto.module').then( m => m.EditarProductoPageModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'nuevo-producto',
+    loadChildren: () => import('./pages/nuevo-producto/nuevo-producto.module').then( m => m.NuevoProductoPageModule)
+  },
+  {
+    path: 'producto-detalle',
+    loadChildren: () => import('./pages/producto-detalle/producto-detalle.module').then( m => m.ProductoDetallePageModule)
+  },
+  {
+    path: 'registro',
+    loadChildren: () => import('./pages/registro/registro.module').then( m => m.RegistroPageModule)
+  },
+  {
+    path: 'user',
+    loadChildren: () => import('./pages/user/user.module').then( m => m.UserPageModule)
+  },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ CrudIonicSpring/frontend/src/app/pages/login/login.page.ts
@@ -0,0 +1,82 @@
+import { Component, OnInit } from '@angular/core';
+import { LoginUsuario } from 'src/app/models/login-usuario';
+import { AuthService } from 'src/app/services/auth.service';
+import { TokenService } from 'src/app/services/token.service';
+import { AlertController } from '@ionic/angular';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+  form: any = {};
+  usuario: LoginUsuario;
+  nombreUser: string;
+  isLogged = false;
+  isLoginFail = false;
+  roles: string[] = [];
+  errorMsg = '';
+
+  constructor(
+    private authService: AuthService,
+    private tokenService: TokenService,
+    private alertController: AlertController,
+    private router: Router
+    ) { }
+
+  ngOnInit() {
+    if (this.tokenService.getToken()) {
+    // comprobamos los valores del token
+      console.log('Nombre: ' + this.tokenService.getUserName());
+      console.log('Token: ' + this.tokenService.getToken());
+      console.log('Roles: ' + this.tokenService.getAuthorities());
+      this.nombreUser = this.tokenService.getUserName();
+      this.isLogged = true;
+      this.isLoginFail = false;
+      this.roles = this.tokenService.getAuthorities();
+      this.router.navigate(['admin'])
+    }
+  }
+
+  onLogin() {
+    this.usuario = new LoginUsuario(this.form.nombreUsuario, this.form.password);
+
+    this.authService.login(this.usuario).subscribe(data => {
+      this.tokenService.setToken(data.token);
+      this.tokenService.setUserName(data.nombreUsuario);
+      this.tokenService.setAuthorities(data.authorities);
+
+      this.isLogged = true;
+      this.isLoginFail = false;
+      this.roles = this.tokenService.getAuthorities();
+      window.location.reload();
+      
+    },
+      (err: any) => {
+        console.log(err);
+        this.isLogged = false;
+        this.isLoginFail = true;
+        this.errorMsg = err.error.message;
+        this.presentAlert();
+      }
+    );
+  }
+
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'Fail en el login',
+      message: this.errorMsg,
+      buttons: ['Aceptar']
+    });
+
+    await alert.present();
+  }
+
+onLogout() {
+  this.tokenService.logOut();
+  window.location.reload();
+}
+
+}
